29d849e4ae0679713020586822c627f067ba42a6
Cordsenboyz <46309019+Cordsenboyz@users.noreply.github.com>
2022-11-08 13:18:55 +0100

    Body css Update

--- vueapp/vue-project/src/components/Navbar.vue
+++ vueapp/vue-project/src/components/Navbar.vue
@@ -23,6 +23,7 @@ export default {
         display: flex;
         flex-direction: row;
         align-items: center;
+        box-shadow: 0 15px 30px 0 rgb(0 0 0 / 11%), 0 5px 15px 0 rgb(0 0 0 / 8%);
     }
     .navbar-h1{
         font-family: sans-serif;
