beda4167846430f584e09a5e9aa9720c570193d5
Cordsenboyz <46309019+Cordsenboyz@users.noreply.github.com>
2022-11-08 12:45:44 +0100

    Implement Navbar Update

---
+++ vueapp/vue-project/src/components/NavItems.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template lang="">
+    <div class="NavItems">
+        <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+            <RouterLink to="/worker">Worker</RouterLink>
+        </nav>
+
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    .NavItems nav a{
+        color: aliceblue;
+        text-decoration: none;
+        font-weight: bold;
+        font-family: sans-serif;
+        width: 60%;
+    }    
+    .NavItems nav a:not(:first-child){
+        padding-left: 1rem;
+        border-left: 2px solid rgb(68, 68, 68);
+    }    
+    .NavItems nav a:not(:last-child){
+        padding-right: 1rem;
+    }
+    .NavItems nav a:hover{
+        color: orange;
+    }
+    
+    .NavItems{
+        margin: 0 auto;
+    }
+
+</style>
---
+++ vueapp/vue-project/src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<script setup>
+import NavItems from './NavItems.vue';
+</script>
+
+<template lang="">
+    <div class="navbar">
+        <h1 class="navbar-h1">Opgavestyring</h1>
+        <NavItems />
+        <h1 class="navbar-temp">LOGIN TEMP</h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    .navbar{
+        top: 0;
+        width: 100%;
+        background-color: rgb(34, 144, 247);
+        height: 3.5rem;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+    .navbar-h1{
+        font-family: sans-serif;
+        padding-left: 5px;
+        color: white;
+        left: 0;
+        width: 20%;
+    }
+    .navbar-temp{
+        font-family: sans-serif;
+        padding-right: 5px;
+        color: white;
+        right: 0;
+        width: 20%;
+        text-align: right;
+    }
+</style>
